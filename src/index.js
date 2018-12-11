c5024e1665e159d7a842015ddf00bb5fb1e0ca97
yurasheremeta <sheremeta.yura@gmail.com>
2018-12-11 11:38:53 +0200

    add functions that calculate positin of image by x , y

--- src/index.js
+++ src/index.js
@@ -1,12 +1,164 @@
 import React from 'react';
+import styled, { css } from 'styled-components';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+
+const Container = styled.div.attrs({
+    style: ({ x, y }) => ({
+     transform: `translate(${x}px, ${y}px)`
+    }),
+  })`
+    cursor: grab;
+
+    ${({ isDragging }) =>
+    isDragging && css`
+      opacity: 0.8;
+      cursor: grabbing;
+    `};
+  `;
+
+  const Image = styled.img.attrs({
+    style: ({ x, y }) => ({
+        transform: `translate(${x}px, ${y}px)`
+       }),
+  })` 
+    cursor: grab;
+
+    ${({ isDragging }) =>
+    isDragging && css`
+      opacity: 0.8;
+      cursor: grabbing;
+    `};
+  `;
+
+  /// зробити обєкт item в ньому x, y і масив зі стейтами 
+
+
+export class App extends React.Component {
+  state = {
+    isDragging: false,
+
+    originalX: 0,
+    originalY: 0,
+
+    translateX: 0,
+    translateY: 0,
+
+    lastTranslateX: 0,
+    lastTranslateY: 0
+  };
+
+  componentWillUnmount() {
+    window.removeEventListener('mousemove', this.handleMouseMove);
+    window.removeEventListener('mouseup', this.handleMouseUp);
+  }
+  handleMouseDown = ({ clientX, clientY }) => {
+    window.addEventListener('mousemove', this.handleMouseMove);
+    window.addEventListener('mouseup', this.handleMouseUp);
+
+    if (this.props.onDragStart) {
+      this.props.onDragStart();
+    }
+
+    this.setState({
+      originalX: clientX,
+      originalY: clientY,
+      isDragging: true
+    });
+    console.log("starting x: ", this.state.translateX , "starting y: ", this.state.translateY);
+
+
+  };
+
+  handleMouseMove = ({ clientX, clientY }) => {
+    const { isDragging } = this.state;
+    const { onDrag } = this.props;
+
+    if (!isDragging) {
+      return;
+    }
+
+    this.setState(prevState => ({
+      translateX: clientX - prevState.originalX + prevState.lastTranslateX,
+      translateY: clientY - prevState.originalY + prevState.lastTranslateY
+    }), () => {
+      if (onDrag) {
+        onDrag({
+          translateX: this.state.translateX,
+          translateY: this.state.translateY
+        });
+      }
+    });
+    console.log("x:" , this.state.translateX, " y:", this.state.translateY);
+
+  };
+
+  handleMouseUp = () => {
+    window.removeEventListener('mousemove', this.handleMouseMove);
+    window.removeEventListener('mouseup', this.handleMouseUp);
+
+    this.setState(
+      {
+        originalX: 0,
+        originalY: 0,
+        lastTranslateX: this.state.translateX,
+        lastTranslateY: this.state.translateY,
+
+        isDragging: false
+      },
+      () => {
+        if (this.props.onDragEnd) {
+          this.props.onDragEnd();
+        }
+      }
+    );
+
+    // img: {
+    //     x,
+    //     y,
+    //     isDra;
+    //     handlehcbc : this.handleMouseDown
+    // }
+    console.log("end point x: ", this.state.lastTranslateX,"end point y: " , this.state.lastTranslateY );
+  };
+
+  render() {
+     const { children } = this.props;
+    const { translateX, translateY, isDragging } = this.state;
+
+    return (
+        <div>
+        <img alt="" src="./favicon.ico" onMouseDown={this.handleMouseDown}/>
+        
+            <div>
+           
+            <Container
+        onMouseDown={this.handleMouseDown}
+        x={translateX}
+        y={translateY}
+        isDragging={isDragging}
+      >
+       <Image src="./favicon.ico"/>
+       {children}
+        </Container>
+
+           <Container
+        onMouseDown={this.handleMouseDown}
+        x={translateX}
+        y={translateY}
+        isDragging={isDragging}
+      >
+       <Image src="./favicon.ico"/>
+       {children}
+        </Container>
+            </div>
+      </div>
+
+    );
+  }
+}
+
+
+
+// // Put the things into the DOM!
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
